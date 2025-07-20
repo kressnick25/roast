492b87c7e2dba062e52b7b24836ee46290f72317
Nicholas <kressnick25@gmail.com>
2025-07-20 20:18:25 +1000

    chore(lint): remove deprecated function usages

--- benches/roast_perf_test.rs
+++ benches/roast_perf_test.rs
@@ -2,9 +2,10 @@
 #![allow(dead_code,unused_imports)]
 
 use std::path::PathBuf;
+use std::hint::black_box;
 
 use criterion::BenchmarkId;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 #[path = "../src/sort.rs"]
 mod sort;
